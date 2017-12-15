d5cbb0915effc26a35dd5b7711ad8f3917927a07
LboAnn <extend1994@gmail.com>
2017-12-15 09:40:07 +0800

    Add required tools for cdnjs project

--- run_envSet.sh
+++ run_envSet.sh
@@ -1,5 +1,7 @@
 # Install the required tools
-sudo apt install git tmux nvm -y
+sudo apt install git tmux wget curl -y
+# 0.33.8 is the latest version of nvm on Dec 15, 2017
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 # Use node v4 managed by nvm
 sudo nvm install v4
 # Use the referenced env setting from PeterDaveHello
@@ -12,3 +14,16 @@ git remote add origin git://cdnjs.peterdavehello.org/cdnjs
 git remote add g-origin https://github.com/cdnjs/cdnjs.git
 git remote add myorigin git@github.com:extend1994/cdnjs.git
 git pull origin master --depth 10
+npm install
+# Get the git auto-update repository
+cd ../
+git clone https://github.com/cdnjs/autoupdate.git && cd autoupdate
+npm install
+# Get minifying tool
+cd ../
+git clone https://github.com/PeterDaveHello/web-minify-helper.git && cd web-minify-helper
+git submodule update --init
+npm install
+# Get tools for adding library & reviewing pull requests
+cd ../
+git clone https://github.com/extend1994/Tools.git
