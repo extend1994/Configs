581f79cdb3caf7e53046b7507ffcdf2f36d071d2
LboAnn <extend1994@gmail.com>
2018-05-18 14:28:56 +0800

    Upgrade node to v8

--- run_envSet.sh
+++ run_envSet.sh
@@ -3,9 +3,9 @@ sudo apt install git tmux wget curl -y
 # 0.33.8 is the latest version of nvm on Dec 15, 2017
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 # Use node v4 managed by nvm
-sudo nvm install v4
+sudo nvm install v8
 # Use the referenced env setting from PeterDaveHello
-curl -Lo- https://goo.gl/FGs2Fu | bas
+curl -Lo- https://goo.gl/FGs2Fu | bash
 # Use my own git config
 # Initilize cdnjs repository
 git init cdnjs && cd cdnjs
